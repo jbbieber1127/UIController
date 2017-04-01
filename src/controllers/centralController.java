2a71418ec0284973358eefa98c6ff30e75389988
N11PFSE-MBPRO\Leon Zhang <zhanghaofan999@gmail.com>
2017-04-01 16:37:16 -0400

    Slight adjustment to title of stage and overall resolution

--- src/controllers/centralController.java
+++ src/controllers/centralController.java
@@ -9,12 +9,12 @@ import javafx.stage.Stage;
 public class centralController  {
   public void startUI (Stage primaryStage) throws Exception {
     loadScene(primaryStage, "../fxmls/AdminLogin.fxml");
-    primaryStage.setTitle("running");
+    primaryStage.setTitle("Faulkner Hospital Kiosk");
   }
 
   public void loadScene (Stage primaryStage, String fxmlpath) throws Exception {
     Parent root = FXMLLoader.load(getClass().getResource(fxmlpath));
-    primaryStage.setScene(new Scene(root, 600, 400));
+    primaryStage.setScene(new Scene(root, 1280, 720));
     primaryStage.show();
   }
 
